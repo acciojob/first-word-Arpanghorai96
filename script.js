4b4beb9775df5f42189eaeadff7cf32d4afc8a3c
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-05-03 12:07:17 -0700

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function firstWord(s) {
   // your code here
 	s=s.trim();
 	s=s.split(" ");
+	if(s.length<0)
+		return "";
 	return s[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
